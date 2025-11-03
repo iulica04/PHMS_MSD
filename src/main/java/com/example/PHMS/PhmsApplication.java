76ee6e621b888f51cc2c77b563fd8c2c073ee262
iulica04 <ivreme12@gmail.com>
2025-11-03 23:34:26 +0200

    initial commit

---
+++ src/main/java/com/example/PHMS/PhmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.PHMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PHMS/PhmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PHMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
